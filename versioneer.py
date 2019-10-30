360dfa25b3d6156570c0559bc3d83aec7efae759
Mark Harfouche <mark.harfouche@gmail.com>
2019-10-30 09:24:57 -0400

    Ensure that dirty trees are included in the version number

--- versioneer.py
+++ versioneer.py
@@ -1,4 +1,4 @@
 import subprocess
 
-revision = subprocess.check_output(["git", "describe", "HEAD"]).strip()
+revision = subprocess.check_output(["git", "describe", "--dirty"]).strip()
 print('-DGIT_DESCRIBE=\'"%s"\'' % revision)

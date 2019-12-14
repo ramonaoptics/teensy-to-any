b7f1d2c80784788508f4bb26b28858c3d3bee175
Mark Harfouche <mark.harfouche@gmail.com>
2019-12-14 13:50:32 -0500

    Use git describe tag

--- versioneer.py
+++ versioneer.py
@@ -1,5 +1,5 @@
 import subprocess
 
-revision = subprocess.check_output(["git", "describe", "--dirty"]).strip()
+revision = subprocess.check_output(["git", "describe", "--tags", "--dirty"]).strip()
 revision = revision.decode()
 print('-DGIT_DESCRIBE=\'"%s"\'' % revision)

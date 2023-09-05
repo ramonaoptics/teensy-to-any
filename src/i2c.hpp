160fc120ead0e7a1908ca38325cab6a5a8344c5f
jed <jed@ramonaoptics.com>
2023-09-05 13:31:32 -0400

    get things to compile

--- src/i2c.hpp
+++ src/i2c.hpp
@@ -22,10 +22,37 @@
 #warning I2C library not available for this build.
 #endif
 
-#if TEENSY_TO_ANY_HAS_I2C
+#if TEENSY_TO_ANY_HAS_I2C_T3
 #include <unistd.h>
 
 class I2CMaster {
+public:
+  int init(int baudrate, int timeout_ms, int address_size,
+           int address_msb_first);
+  int reset();
+  int write_uint16(int slave_address, int register_address, uint16_t data);
+  int read_uint16(int slave_address, int register_address, uint16_t &data);
+  int write_uint8(int slave_address, int register_address, uint8_t data);
+  int read_uint8(int slave_address, int register_address, uint8_t &data);
+  int read_no_register_uint8(int slave_address, uint8_t &data);
+  int write_no_register_uint8(int slave_address, uint8_t data);
+  int write_payload(int slave_address, int register_address, uint8_t *data,
+                    int num_bytes);
+  int read_payload(int slave_address, int register_address, uint8_t *data,
+                   int num_bytes);
+
+private:
+  void _write_register_address(int register_address);
+  int address_msb_first = 0;
+  int address_size = 0;
+  bool is_initialized = false;
+  bool slave_8bit_address = true;
+};
+#endif
+
+#if TEENSY_TO_ANY_HAS_I2C_T4
+#include <unistd.h>
+class I2CMaster_T4 {
 public:
   int init(int baudrate, int timeout_ms, int address_size,
            int address_msb_first);

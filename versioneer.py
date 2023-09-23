2a1a80929bdd7c8c12e23eb97d5b72595b958271
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-23 19:16:01 -0400

    Make things Version compliant in python

--- versioneer.py
+++ versioneer.py
@@ -2,4 +2,6 @@ import subprocess
 
 revision = subprocess.check_output(["git", "describe", "--tags", "--dirty"]).strip()
 revision = revision.decode()
+revision = revision.replace('-', '.dev', 1)
+revision = revision.replace('-', '+', 1)
 print('-DGIT_DESCRIBE=\'"%s"\'' % revision)

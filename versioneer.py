96895cfe60c3a4449d266767ef769eb80ace97f7
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-29 08:21:48 -0400

    Fix edge case on dirty versions

--- versioneer.py
+++ versioneer.py
@@ -2,6 +2,8 @@ import subprocess
 
 revision = subprocess.check_output(["git", "describe", "--tags", "--dirty"]).strip()
 revision = revision.decode()
+revision = revision.replace('-dirty', '+dirty', 1)
 revision = revision.replace('-', '.dev', 1)
-revision = revision.replace('-', '+', 1)
+revision = revision.replace('-g', '+g', 1)
+
 print('-DGIT_DESCRIBE=\'"%s"\'' % revision)

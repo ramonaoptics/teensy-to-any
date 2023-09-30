4894ebcc4030595ea25a0740137998f516c29195
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-30 15:52:22 -0400

    Use post instead of dev to make sure version comparision is consistent

--- versioneer.py
+++ versioneer.py
@@ -3,7 +3,7 @@ import subprocess
 revision = subprocess.check_output(["git", "describe", "--tags", "--dirty"]).strip()
 revision = revision.decode()
 revision = revision.replace('-dirty', '+dirty', 1)
-revision = revision.replace('-', '.dev', 1)
+revision = revision.replace('-', '.post', 1)
 revision = revision.replace('-g', '+g', 1)
 
 print('-DGIT_DESCRIBE=\'"%s"\'' % revision)

f6b90e3f38b782b32eff578e07cade1ce8080b01
Mark Harfouche <mark.harfouche@gmail.com>
2025-03-03 09:39:47 -0500

    Document what the different builds are

--- src/examples/fan_hub_default_off/fan_hub.cpp
+++ src/examples/fan_hub_default_off/fan_hub.cpp
@@ -1,19 +1,9 @@
-/* A small demo to show how the teensy-to-any can be used to control 4 pin fans.
- * It is assumed that the fan PWM pin is connected to pin 2
- * and that an indicator LED is connected to Pin 13 (this is the default board)
+/* Setup the Teensy 4 to control a 4 pin PWM fan on pin 2.
  *
- * The demo code will run the fan at
- *  * 100%
- *  * 75%
- *  * 50%
- *  * 25%
- *  * 0%
- * For 3 seconds at a time and then continue in an infinite loop.
- * The LED can be used as a visual guide of the expected speed of the fan.
+ * After bootup, the Teensy is ready to control the fan, however
+ * the fan is set to off.
  *
- * Not all fan will be able to operate at 25% duty cyle.
- *
- * Ramona Optics - 2024
+ * Ramona Optics - 2024-2025
  */
 const char *teensy_to_any_startup_commands[] = {
     // Setup the LED as an indicator pin
@@ -31,36 +21,5 @@ const char *teensy_to_any_startup_commands[] = {
 
 // Change the fan speed
 const char *teensy_to_any_demo_commands[] = {
-    // "analog_write 2 255",
-    // "analog_write 13 255",
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-
-    // "analog_write 2 192",
-    //  // Brightness is a strange thing, and does not
-    //  // decrease so linearly according to human perception
-    // "analog_write 13 128",
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-
-    // "analog_write 2 128",
-    // "analog_write 13 30",
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-
-    // "analog_write 2 64",
-    // "analog_write 13 10",
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-
-    // "analog_write 2 0",
-    // "analog_write 13 0",
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
-    // "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", "sleep 0.2", // 1000 ms
     nullptr,
 };

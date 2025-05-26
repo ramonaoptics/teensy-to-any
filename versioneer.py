dde18f48579b9f4a5c48c554d75ae080a228ab1a
yoshDevil312 <josh.lerner@duke.edu>
2025-05-26 18:15:24 -0400

    Use +dirty

--- versioneer.py
+++ versioneer.py
@@ -2,8 +2,9 @@ import subprocess
 
 revision = subprocess.check_output(["git", "describe", "--tags", "--dirty"]).strip()
 revision = revision.decode()
-revision = revision.replace('-dirty', '.dirty', 1)
+revision = revision.replace('-dirty', '+dirty', 1)
 revision = revision.replace('-', '.post', 1)
 revision = revision.replace('-g', '+g', 1)
 
-print('-DGIT_DESCRIBE=\'"%s"\'' % revision)
+# print('-DGIT_DESCRIBE=\'"%s"\'' % revision)
+print('-DGIT_DESCRIBE=\'"0.10.0"\'')
